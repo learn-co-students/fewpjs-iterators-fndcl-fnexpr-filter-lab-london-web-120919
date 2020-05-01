f0cd64e2639252d1ec2e5d01781ae5aafd9e1a3b
github username <github email address>
2020-05-01 14:30:58 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+const findMatching = (array,condition) => {
+   return array.filter( item => item.toLowerCase() === condition.toLowerCase())
+}
+
+const fuzzyMatch = (array,condition) => {
+    return array.filter( item => item.toLowerCase()[0] === condition.toLowerCase()[0])
+ }
+ 
+ const matchName = (array,condition) => {
+    return array.filter( item => item.name.toLowerCase() === condition.toLowerCase())
+ }
